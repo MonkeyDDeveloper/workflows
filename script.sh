22c2bc900a52fb4eb9c3eb890febea9f7974a21e
Javier Fray <javier.fray@dacodes.com.mx>
2024-09-19 22:54:34 -0500

    test script

---
+++ script.sh
@@ -0,0 +1,15 @@
+# probar el echo
+echo "hello world"
+
+# actualizar sistema
+sudo apt update
+sudo apt upgrade
+
+# instalar nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+# actualizar el shell
+source ~/.bashrc
+
+# instalar la úlitma versión de node
+nvm install latest
