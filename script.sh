7bd722e967d4496bfc63e76b03719140ded06138
Javier Fray <javier.fray@dacodes.com.mx>
2024-09-19 23:02:17 -0500

    agregar comando para recargar el bash

--- script.sh
+++ script.sh
@@ -3,13 +3,14 @@ echo "hello world"
 
 # actualizar sistema
 sudo apt update
-sudo apt upgrade
+# sudo apt upgrade
 
 # instalar nvm
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
 # actualizar el shell
 source ~/.bashrc
+source ~/.zshrc
 
 # instalar la úlitma versión de node
 nvm install latest
