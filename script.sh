8b60e4e6d3046f299c3c7531bda44a0a6917de9d
Javier Fray <javier.fray@dacodes.com.mx>
2024-09-19 23:09:09 -0500

    agregar scripts de instalación de nvm

--- script.sh
+++ script.sh
@@ -1,16 +1,1 @@
-# probar el echo
-echo "hello world"
-
-# actualizar sistema
-sudo apt update
-# sudo apt upgrade
-
-# instalar nvm
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-
-# actualizar el shell
-source ~/.bashrc
-source ~/.zshrc
-
-# instalar la úlitma versión de node
 nvm install latest
