7860c3521c5c8c27f505d07aec2f5648a976b37a
Javier Fray <javier.fray@dacodes.com.mx>
2024-10-01 14:44:33 -0500

    adding first version of the backend

---
+++ back/PortfolioBackend/PortfolioBackend/Program.cs
@@ -0,0 +1,35 @@
+using DotNetEnv;
+Env.Load();
+
+var testingVar = Environment.GetEnvironmentVariable("MY_FIRST_ENV_VARIABLE");
+
+if(testingVar != null)
+{
+    Console.WriteLine(testingVar);
+}
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
