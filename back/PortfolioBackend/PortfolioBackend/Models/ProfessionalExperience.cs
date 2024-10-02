8e4da4287c8183d58507ef558522a835cce2b91d
Javier Fray <javier.fray@dacodes.com.mx>
2024-10-01 23:41:47 -0500

    CREATE model ProffesionalExperience

---
+++ back/PortfolioBackend/PortfolioBackend/Models/ProfessionalExperience.cs
@@ -0,0 +1,19 @@
+﻿namespace PortfolioBackend.Models
+{
+    public class ProfessionalExperience
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+
+        public required string Description { get; set; }
+
+        public required DateOnly StartDate { get; set; }
+
+        public required DateOnly EndDateOnly { get; set; }
+
+        public required List<string> Responsabilities { get; set; }
+
+        public required List<string> Tecnologies { get; set; }
+
+    }
+}
