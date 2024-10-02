da36ca13b4abca462f9c556044e1a2a9f4172a82
Javier Fray <javier.fray@dacodes.com.mx>
2024-10-01 23:42:25 -0500

    ADDING first version of Context for db

---
+++ back/PortfolioBackend/PortfolioBackend/Context/PortfolioDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using PortfolioBackend.Models;
+
+namespace PortfolioBackend.Context
+{
+    public class PortfolioDbContext : DbContext
+    {
+        public PortfolioDbContext(DbContextOptions<PortfolioDbContext> builderOptions) : base(builderOptions)
+        {
+            
+        }
+
+        public DbSet<ProfessionalExperience> ProfessionalExperiences { get; set; }
+    }
+}
