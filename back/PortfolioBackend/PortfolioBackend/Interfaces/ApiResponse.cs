77303483aafe81d77264c12a89f89630f8066e84
Javier Fray <javier.fray@dacodes.com.mx>
2024-10-02 19:37:12 -0500

    AÑADIR interfaz de respuesta

---
+++ back/PortfolioBackend/PortfolioBackend/Interfaces/ApiResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace PortfolioBackend.Interfaces
+{
+    public interface ApiResponse<T>
+    {
+        string Message { get; set; }
+        string Detail { get; set; }
+        T Data { get; set; }
+    }
+}
