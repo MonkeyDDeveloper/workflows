2d9477e4dcd95582f40e5f8b125299baec14d75a
Javier Fray <javier.fray@dacodes.com.mx>
2024-10-04 22:17:52 -0500

    ADD next.config file

---
+++ front/portfolio/next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    reactStrictMode: true,
+    i18n: {
+        locales: ['es', 'en'],
+        defaultLocale: 'en',
+    },
+    compiler: {
+        styledComponents: true,
+    }
+};
