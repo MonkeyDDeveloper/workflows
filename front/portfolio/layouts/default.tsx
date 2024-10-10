75815be8e990c4b93f852308d6a996fcba7ddcb4
Javier Fray <javier.fray@dacodes.com.mx>
2024-10-10 15:23:02 -0500

    ACTUALIZAR layout

--- front/portfolio/layouts/default.tsx
+++ front/portfolio/layouts/default.tsx
@@ -3,7 +3,7 @@ import Navigation from "../components/common/Navigation"
 export default function Layout({ children }) {
     return (
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <section className="w-12/12 md:w-8/12 mx-auto">
+            <section className="w-12/12 md:w-8/12 mx-auto duration-300 bg-gradient-to-b dark:from-black dark:from-5% dark:to-cyan-900 dark:to-100% min-h-screen">
                 <Navigation />
                 <main>
                     {children}
