527f75eef7a35034a7b42ca7dc30cc934fe37b2b
Javier Fray <javier.fray@dacodes.com.mx>
2024-10-13 14:08:07 -0500

    INIT experience page

--- front/portfolio/pages/portfolio/experience/index.tsx
+++ front/portfolio/pages/portfolio/experience/index.tsx
@@ -2,21 +2,26 @@ import { useEffect } from "react"
 import { portofioApi } from "../../../services/portfolio.services"
 import { ProfessionalExperience } from "../../../models/ProfessionalExperience"
 import { useTranslations } from "next-intl"
-
-async function getExperiences() {
-    const uri = `${process.env.NEXT_PUBLIC_BACK_API_URI}/professional-experiences`
-    console.log({ uri })
-    const { message, data } = await portofioApi.get<ProfessionalExperience[]>(uri)
-    if (!data) {
-        console.error(`Error consultando las experiencias profesionales: ${message}`)
-        return
-    }
-    console.log({ data })
-    return data
-}
+import { motion } from "framer-motion"
+import ExperienceCard from "./components/ExperienceCard"
+import { useState } from "react"
 
 export default function Index() {
 
+    async function getExperiences() {
+        const uri = `${process.env.NEXT_PUBLIC_BACK_API_URI}/professional-experiences`
+        console.log({ uri })
+        const { message, data } = await portofioApi.get<ProfessionalExperience[]>(uri)
+        if (!data) {
+            console.error(`Error consultando las experiencias profesionales: ${message}`)
+            return
+        }
+        const reversedData = data.reverse()
+        console.log({ data })
+        setExperiences(reversedData)
+    }
+
+    const [experiences, setExperiences] = useState<ProfessionalExperience[]>([])
     const t = useTranslations()
 
     useEffect(() => {
@@ -24,8 +29,32 @@ export default function Index() {
     }, [])
 
     return (
-        <section>
-            <h1>{t('experience.title')}</h1>
+        <section className="w-11/12 mx-auto">
+            <h1 className="font-minecraft text-3xl text-orange-600">
+                {t('experience.title')}!
+            </h1>
+            <motion.p className="mt-3" initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 0.5 }}>
+                <span className="font-minecraft">
+                    {t('experience.description')}
+                </span>
+            </motion.p>
+            <motion.p initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 0.5 }}>
+                <span className="font-minecraft">
+                    {t('experience.description2')}
+                </span>
+            </motion.p>
+            <section className="mt-3">
+                {/* grid de una columna para pantallas pequeñas y responsive para pantallas más grandes */}
+                <motion.div className="grid grid-cols-1 md:grid-cols-2 gap-3">
+                    {
+                        experiences.map((experience, index) => {
+                            return (
+                                <ExperienceCard key={index} experience={experience} />
+                            )
+                        })
+                    }
+                </motion.div>
+            </section>
         </section>
     )
 }
