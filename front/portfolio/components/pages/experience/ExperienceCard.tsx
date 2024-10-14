9d91d1af5f13c40dd04a4d308d6da570ac2cd533
Javier Fray <javier.fray@dacodes.com.mx>
2024-10-14 11:24:47 -0500

    UPDATE ExperienceCard to show "presen" on current pe

--- front/portfolio/components/pages/experience/ExperienceCard.tsx
+++ front/portfolio/components/pages/experience/ExperienceCard.tsx
@@ -1,14 +1,17 @@
 import { ProfessionalExperience } from "@/models/ProfessionalExperience";
 import { FaDocker } from "react-icons/fa";
 import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
+import { useTranslations } from "next-intl";
 
 export default function ExperienceCard({ experience }: { experience: ProfessionalExperience }) {
+    const t = useTranslations()
+
     return (
         <Card className="py-4">
             <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
                 <h4 className="text-large uppercase font-bold mb-1 font-minecraft">{experience.title}</h4>
                 <small className="text-default-500">{experience.description}</small>
-                <p className="font-bold text-tiny">{experience.startDate} - {experience.endDate}</p>
+                <p className="font-bold text-tiny">{experience.startDate} - {experience.isCurrent ? t("experience.present") : experience.endDate}</p>
             </CardHeader>
             <CardBody className="overflow-visible py-2">
                 <Image
@@ -20,4 +23,12 @@ export default function ExperienceCard({ experience }: { experience: Professiona
             </CardBody>
         </Card>
     );
+}
+
+export async function getStaticProps(context) {
+    return {
+        props: {
+            messages: (await import(`@/public/locales/${context.locale}.json`)).default
+        }
+    };
 }
