09212234a2dfdc81c82bb10ae399f118dad537ea
Javier Fray <javier.fray@dacodes.com.mx>
2024-10-15 18:33:28 -0500

    UPDATE Experience card accordion

--- front/portfolio/components/pages/experience/ExperienceCardAccordion.tsx
+++ front/portfolio/components/pages/experience/ExperienceCardAccordion.tsx
@@ -28,6 +28,7 @@ export default function ExperienceCardAccordion({ responsibilites = [], technolo
             selectedKeys={expandedAccordionItems}
             onSelectionChange={(keys) => setExpandedAccordionItems(Array.from(keys as Set<string>))}
             itemClasses={itemClasses}
+            selectionMode="multiple"
         >
             <AccordionItem key="1" aria-label={t("experience.accordion.responsibilities")} title={t("experience.accordion.responsibilities")}>
                 <ul className="list-disc md:p-1">
