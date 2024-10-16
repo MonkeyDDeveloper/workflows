ad95d2a339db10bf26c1dd38431b0bcc64dbae35
Javier Fray <javier.fray@dacodes.com.mx>
2024-10-16 13:57:51 -0500

    ADD footer on layout

--- front/portfolio/layouts/default.tsx
+++ front/portfolio/layouts/default.tsx
@@ -1,13 +1,15 @@
 import { ThemeProvider } from "next-themes"
 import Navigation from "../components/common/Navigation"
+import Footer from "@/components/common/Footer"
 export default function Layout({ children }) {
     return (
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <section className="w-12/12 md:w-8/12 md:outline outline-offset-2 outline-cyan-600 dark:outline-white mx-auto duration-300 bg-gradient-to-b from-white to-blue-300 dark:from-black from-20% dark:to-cyan-900 dark:to-100% min-h-screen">
+            <section className="w-12/12 md:w-8/12 md:outline outline-offset-2 outline-cyan-600 dark:outline-white mx-auto duration-300 bg-gradient-to-b from-white to-blue-300 dark:from-black from-20% dark:to-cyan-900 dark:to-100% min-h-screen flex flex-col flex-wrap">
                 <Navigation />
-                <main>
+                <main className="flex-1">
                     {children}
                 </main>
+                <Footer />
             </section>
         </ThemeProvider>
     )
