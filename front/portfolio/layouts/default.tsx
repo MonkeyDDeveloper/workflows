0937e5a46d9a618b76c997e5f222a152cc41c8f5
Javier Fray <javier.fray@dacodes.com.mx>
2024-10-16 20:01:58 -0500

    MOVE icon to another folder

--- front/portfolio/layouts/default.tsx
+++ front/portfolio/layouts/default.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }) {
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             <section className="w-12/12 md:w-8/12 md:outline outline-offset-2 outline-cyan-600 dark:outline-white mx-auto duration-300 bg-gradient-to-b from-white to-blue-300 dark:from-black from-20% dark:to-cyan-900 dark:to-100% min-h-screen flex flex-col flex-wrap">
                 <Head>
-                    <link rel="icon" href="/favicon.ico" />
+                    <link rel="icon" href="/favicon/favicon.ico" />
                 </Head>
                 <Navigation />
                 <main className="flex-1">
