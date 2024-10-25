9ac5e094fa1584bf68d6865204acbcff5ce6d7df
Javier Fray <javier.fray@dacodes.com.mx>
2024-10-25 18:06:09 -0500

    update footer

--- front/portfolio/components/common/Footer.tsx
+++ front/portfolio/components/common/Footer.tsx
@@ -2,7 +2,9 @@ import { BsLinkedin } from "react-icons/bs"
 import { BsGithub } from "react-icons/bs"
 import { BsWhatsapp } from "react-icons/bs"
 import { FaFileAlt } from "react-icons/fa"
-export default function Footer() {
+import { motion } from "framer-motion"
+
+export default function Footer() {    
     return (
         <div className="p-2 pb-4 border-t border-cyan-600 bg-transparent">
             <p className="text-lg text-center mb-3 font-minecraft">Javier Fray</p>
@@ -20,7 +22,10 @@ export default function Footer() {
                     <FaFileAlt className="mx-1 w-6 h-6" />
                 </a>
             </section>
-            <p className="text-sm text-center mt-3 font-minecraft">© 2024 Mayra ❤️</p>
+            <audio id="audio" controls>
+                <source type="audio/mp3" src="/audio/potter.mp3" />
+            </audio>
+            <p className="text-sm text-center mt-3 font-minecraft">© 2024 <span className="inline-block m-0 opacity-0 hover:opacity-100 hover:cursor-pointer">Mayra <motion.span className="inline-block m-0" whileInView={{ scale: [1, 1.1, 1], transition: { duration: 1.1, repeat: Infinity } }}>❤️</motion.span></span></p>
         </div>
     )
 }
